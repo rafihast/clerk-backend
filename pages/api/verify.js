0769c2476735c48369c58f039b5878b561a6602c
rafihastiar <90260976+rafihastiar@users.noreply.github.com>
2025-07-21 17:55:33 +0700

    nbb

--- pages/api/verify.js
+++ pages/api/verify.js
@@ -1,6 +1,6 @@
 // File: pages/api/verify.js
 import { clerkClient } from "@clerk/clerk-sdk-node";
-import { withAuth } from '@clerk/nextjs/api';
+import { withAuth } from "@clerk/nextjs/server"; 
 
 const handler = async (req, res) => {
   console.log("🔵 [API] Request masuk ke /api/verify");
