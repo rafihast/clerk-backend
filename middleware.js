dbd257918193ac20e76c38a147b06ba545e503d8
rafihastiar <90260976+rafihastiar@users.noreply.github.com>
2025-07-21 17:00:13 +0700

    tes

--- middleware.js
+++ middleware.js
@@ -1,7 +1,17 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-export default clerkMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/", "/api/verify", "/api/hello"],
+  ignoredRoutes: [],
+});
 
 export const config = {
-  matcher: ["/api/:path*"]
+  matcher: [
+    /*
+     * Apply middleware to all routes except:
+     * - static files (_next, favicon, etc)
+     * - public routes
+     */
+    "/((?!_next|favicon.ico|api/verify|api/hello|public).*)",
+  ],
 };
